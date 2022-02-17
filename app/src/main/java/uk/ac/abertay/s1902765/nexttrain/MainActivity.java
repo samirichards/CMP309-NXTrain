256da355a7f63303266453931b77f0fe116ee426
SAMI RICHARDS <samirichards100@gmail.com>
2022-02-17 19:41:37 +0000

    First Commit

---
+++ app/src/main/java/uk/ac/abertay/s1902765/nexttrain/MainActivity.java
@@ -0,0 +1,14 @@
+package uk.ac.abertay.s1902765.nexttrain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
