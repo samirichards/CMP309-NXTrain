4ae867c3e62a4349290b180fbcc28da43ad71112
SAMI RICHARDS <samirichards100@gmail.com>
2022-04-01 03:32:29 +0100

    Yep

--- app/src/main/java/uk/ac/abertay/s1902765/nexttrain/MainActivity.java
+++ app/src/main/java/uk/ac/abertay/s1902765/nexttrain/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigation = findViewById(R.id.home_bottom_nav);
+        bottomNavigation.setupWithNavController(Navigation.findNavController(this, R.id.homeNavHost));
+
     }
 }
