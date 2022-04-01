d3eb20774637532ba4096c7998849aa902e332df
SAMI RICHARDS <samirichards100@gmail.com>
2022-04-01 04:44:59 +0100

    Bottom navigation between fragments is now functional

--- app/src/main/java/uk/ac/abertay/s1902765/nexttrain/MainActivity.java
+++ app/src/main/java/uk/ac/abertay/s1902765/nexttrain/MainActivity.java
@@ -1,8 +1,16 @@
 package uk.ac.abertay.s1902765.nexttrain;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.BottomNavigationViewKt;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -13,8 +21,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView bottomNavigation = findViewById(R.id.home_bottom_nav);
-        bottomNavigation.setupWithNavController(Navigation.findNavController(this, R.id.homeNavHost));
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.homeNavHost);
+        NavController navController = navHostFragment.getNavController();
 
+        NavigationBarView homeBottomNavBar = findViewById(R.id.home_bottom_nav);
+        NavigationUI.setupWithNavController(homeBottomNavBar, navController);
     }
 }
