64a2a6e8bff27d9930bc67f134f37c1219026853
SAMI RICHARDS <samirichards100@gmail.com>
2022-04-09 09:29:12 +0100

    Stuff still needs finishing

--- app/src/main/java/uk/ac/abertay/s1902765/nexttrain/LiveTrains.java
+++ app/src/main/java/uk/ac/abertay/s1902765/nexttrain/LiveTrains.java
@@ -2,21 +2,30 @@ package uk.ac.abertay.s1902765.nexttrain;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.Observable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
+
+import java.util.List;
+import java.util.Locale;
 
 import uk.ac.abertay.s1902765.nexttrain.databinding.FragmentLiveTrainsBinding;
+import uk.ac.abertay.s1902765.nexttrain.databinding.StationSearchitemLayoutBinding;
 
 
 public class LiveTrains extends Fragment {
 
     private LiveTrains_Model liveTrainsModel;
     private FragmentLiveTrainsBinding binding;
+    private SearchView liveTrainsSearchView;
 
     public LiveTrains() {
         // Required empty public constructor
@@ -31,14 +40,48 @@ public class LiveTrains extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentLiveTrainsBinding.inflate(inflater, container, false);
+        //Forgot to add this one line, viewmodels now work correctly
+        binding.setLiveTrainsModel(liveTrainsModel);
         View view = binding.getRoot();
         return view;
-        //TODO make the databinding work properly
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+}
+
+class TestStationsRecyclerViewAdapter extends RecyclerView.Adapter{
+    private List<String> items;
+
+    private class StationViewHolder extends RecyclerView.ViewHolder{
+
+        StationSearchitemLayoutBinding binding;//Name of the test_list_item.xml in camel case + "Binding"
+
+        public StationViewHolder(StationSearchitemLayoutBinding b){
+            super(b.getRoot());
+            binding = b;
+        }
+    }
+
+    StationSearchitemLayoutBinding stationBinding;
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new StationViewHolder(StationSearchitemLayoutBinding.inflate(LayoutInflater.from(parent.getContext())));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        String text = String.format(Locale.ENGLISH, "%s %d", items.get(position), position);
+
+        //TODO Ugh, finish this later
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
     }
 }
